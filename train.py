046f25fe0b74bd4850fcae1a93c9052825e51f26
Hikmat Farhat <h.farhat@soton.ac.uk>
2023-07-11 14:40:28 +0100

    working code

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import comet_ml
 import torch
 import torch.nn as nn
 import torchvision.transforms as tf
+from torchvision.utils import  make_grid,save_image
 from PIL import Image
 import torch.autograd as autograd
 from networks import Discriminator,Generator2
@@ -18,6 +19,7 @@ import yaml
 from munch import DefaultMunch
 from tqdm import tqdm
 from comet_ml.integration.pytorch import log_model
+from random import randint
 
 # The images files have the form "ID_2m_0P_xV_yH_z.jpg" where ID is the ID of the person, 2m is fixed, 0P means head pose of 0 degrees (only head pose used in this notebook)
 # x is the vertical orientation, y is the horizontal orientation and z is either L for left or R for right eye (note that the right eye patch was flipped horizontally).
@@ -75,8 +77,6 @@ class PreProcessData():
         self.test_images = []
         self.test_angles_r = []
         self.test_labels = []
-        self.test_images_t = []
-        self.test_angles_g = []
         self.preprocess()
     def preprocess(self):
 
@@ -118,17 +118,16 @@ class PreProcessData():
                         self.train_labels.append(idx - 1)
                         self.train_images_t.append(file_path_t)
                         self.train_angles_g.append([h_angle_g, v_angle_g])
-            if idx > self.ids :
-                self.test_images.append(file_path)
-                self.test_angles_r.append([h_angle_r, v_angle_r])
-                self.test_labels.append(idx - 1)
-                self.test_images_t.append(file_path_t)
-                self.test_angles_g.append([h_angle_g, v_angle_g])
+                if idx > self.ids :
+                    self.test_images.append(file_path)
+                    self.test_angles_r.append([h_angle_r, v_angle_r])
+                    self.test_labels.append(idx - 1)
+                    
     def training_data(self):
             return self.train_images,self.train_angles_r,self.train_labels,self.train_images_t,self.train_angles_g
     def testing_data(self):
-            return self.test_images,self.test_angles_r,self.test_labels,self.test_images_t,self.test_angles_g
-class MyDataset(torch.utils.data.Dataset):
+            return self.test_images,self.test_angles_r,self.test_labels
+class TrainDataset(torch.utils.data.Dataset):
     def __init__(self, images,angles_r,labels,images_t,angles_g, transform=None):
         super().__init__()
         self.transform = transform
@@ -147,14 +146,29 @@ class MyDataset(torch.utils.data.Dataset):
         
     def __len__(self):
         return len(self.images)
+class TestDataset(torch.utils.data.Dataset):
+    def __init__(self, images,angles_r,labels, transform=None):
+        super().__init__()
+        self.transform = transform
+        self.images=images
+        self.angles_r=angles_r
+        self.labels=labels
+    def __getitem__(self, index):
+        return (
+            self.transform(Image.open(self.images[index])),
+                torch.tensor(self.angles_r[index]),
+                self.labels[index])
+        
+    def __len__(self):
+        return len(self.images)
 data=PreProcessData(config.data_path)
 transform=tf.Compose([tf.ToTensor(),tf.Resize((64,64),antialias=True)])
-train_dataset=MyDataset(*data.training_data(),transform=transform)
-test_dataset=MyDataset(*data.testing_data(),transform=transform)
+train_dataset=TrainDataset(*data.training_data(),transform=transform)
+test_dataset=TestDataset(*data.testing_data(),transform=transform)
 
 #dataset=MyDataset(dir_path=config.data_path,transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=config.test_batch_size, shuffle=False)
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
 
@@ -217,7 +231,10 @@ def discriminator_step(generator,discriminator,batch):
 
 
 def recover_image(img):
-    img=img.cpu().numpy().transpose(0, 2, 3, 1)*255
+    if len(img.shape)==3:
+        img=img.cpu().numpy().transpose(1, 2, 0)*255
+    else:
+        img=img.cpu().numpy().transpose(0, 2, 3, 1)*255
     return img.astype(np.uint8)
 def save_images(imgs):
     height=recover_image(imgs[0])[0].shape[0]
@@ -229,6 +246,14 @@ def save_images(imgs):
         result = Image.fromarray(recover_image(img)[0])
         new_im.paste(result, (i*width+i,0))
     return new_im
+
+def prepare_images(listoflists):
+    res=[]
+    for i in range(config.test_batch_size):
+        for l in listoflists:
+            res.append(l[i])
+
+    return res
 # z=0
 # for epoch in tqdm(range(config.epochs)):
 #     for batch in tqdm(train_loader):
@@ -247,34 +272,48 @@ for epoch in loop:
         if batch_count%config.critic_iter_per_gen==0:
             l_g=generator_step(generator,discriminator,loss_network,batch)
             loss_g=0.9*loss_g+0.1*l_g
-        if batch_count%config.image_save_freq==0:
-            imgs=[batch[0]]
-            for h in [-15,-10,-5,0,5,10,15]:
-                    a=torch.tile(torch.tensor([h/15.,0.]),[32,1])
-                    a=a.to(device)
-                    y=generator(batch[0],a)
-                    imgs.append(y.detach())
-            filename="./debug/{}_{}.png".format(epoch,batch_count)
-            im=save_images(imgs)
-            im.save(filename)
-            if config.use_comet is not None:
-                experiment.log_image(im)
+    if epoch%config.image_save_freq==0:
+        # imgs=[batch[0]]
+        # for h in [-15,-10,-5,0,5,10,15]:
+        #         a=torch.tile(torch.tensor([h/15.,0.]),[32,1])
+        #         a=a.to(device)
+        #         y=generator(batch[0],a)
+        #         imgs.append(y.detach())
+        # filename="./debug/{}_{}.png".format(epoch,batch_count)
+        # im=save_images(imgs)
+        # im.save(filename)
+        try:
+            orig=next(test_iter)[0].to(device)
+      
+        except:
+            test_iter=iter(test_loader)
+            orig=next(test_iter)[0].to(device)# extract the images
+        imgs=[torch.unbind(orig,0)]
+        for h in [-15,-10,-5,0,5,10,15]:
+                a=torch.tile(torch.tensor([h/15.,0.]),[config.test_batch_size,1])
+                a=a.to(device)
+                y=generator(orig,a).detach()
+                y=torch.unbind(y,0)
+                imgs.append(y)
+        imgs=prepare_images(imgs)
+        filename="./debug/{}_{}.png".format(epoch,batch_count)
+        img=make_grid(imgs,nrow=8)#nrow specifies the number of columns !!!
+        img=recover_image(img)
+        img=Image.fromarray(img)
+        img.save(filename)
+        #save_image(imgs,filename,nrow=8)# nrow specifies the number of columns !!!
+        if config.use_comet is not None:
+            experiment.log_image(img)
     loop.set_postfix(loss_d=loss_d,loss_g=loss_g)
     print(l_d,l_g)
     if config.use_comet is not None:
         metrics={'loss_d':l_d,'loss_g':l_g}
         experiment.log_metrics(metrics, epoch=epoch)
     if epoch%config.model_save_freq==0:
-        # if config.use_comet is not None:
-        #     log_model(experiment,generator,"generator")
-        #     log_model(experiment,discriminator,"discriminator")
-        # torch.save(generator, './generator.pth')
-        # torch.save(discriminator, './discriminator.pth')
-        orig=next(iter(test_loader))[0].to(device)
-        imgs=[orig]
-        for h in [-15,-10,-5,0,5,10,15]:
-                    a=torch.tile(torch.tensor([h/15.,0.]),[32,1])
-                    a=a.to(device)
-                    y=generator(orig,a)
-                    imgs.append(y.detach())
-     
+        if config.use_comet is not None:
+            log_model(experiment,generator,"generator")
+            log_model(experiment,discriminator,"discriminator")
+        torch.save(generator, './generator.pth')
+        torch.save(discriminator, './discriminator.pth')
+        
+        
